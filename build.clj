0e3f0e6d5b8c125f926b01ad73d8d3614482c50b
Christian Westrom <c.westrom@westrom.xyz>
2022-05-14 14:51:11 -0400

    get a build process working

---
+++ build.clj
@@ -0,0 +1,13 @@
+(ns build
+  (:refer-clojure :exclude [test])
+  (:require [clojure.tools.build.api :as b]
+            [org.corfield.build :as bb]))
+
+(def lib 'wildwestrom/bp-typer)
+(def version (format "0.0.%s" (b/git-count-revs nil)))
+
+(defn ci [opts]
+  (-> opts
+      (assoc :lib lib :version version :main 'bp-typer.core)
+      (bb/clean)
+      (bb/uber)))
--- src/bp_typer/core.clj
+++ src/bp_typer/core.clj
@@ -4,7 +4,8 @@
             [clojure.string :as string]
             [clojure.java.io :as io]
             [clojure.set :as set])
-  (:import (java.util HexFormat)))
+  (:import (java.util HexFormat))
+  (:gen-class))
 
 (defn- create-array-of-tiles [list-of-coords tile-name]
   (mapv (fn [[x y]]
@@ -58,10 +59,6 @@
   (bp-from-tile-array
    (create-array-of-tiles (hex-to-tile-array hex-string) "refined-concrete")))
 
-(defn- char-to-int [char]
-  (assert (= java.lang.Character (type char)))
-  (int char))
-
 (def ^:private unifont-data
   (set (map (fn [s]
           ((fn [[idx val]]
@@ -74,16 +71,13 @@
        (first %))
     (set/select #(= cp (:id %)) unifont-data))))
 
+(defn- char-to-int [char]
+  (assert (= java.lang.Character (type char)))
+  (int char))
+
 (defn- char-to-bp [char]
   (hex-to-bp (grab-data-by-codepoint (char-to-int char))))
 
 (defn -main [& args]
   (assert (= 1 (count args)))
-  (char-to-bp args))
-
-(comment
-  (char-to-bp \工)
-  ;; => "0eJyd1cGOgyAQBuB3mTNNHEQRjn2NZg/Wzm5ILBplN2sa373YNtle9uB/ZfhmEvIDNzr33zJOISbyNwpJruTf1hSFbogz+VMuxov8kmdFc/iKbb+B2F4lg0k+Q5TLIe/tJkmSXVrGrfLouK4fivr2LNnQ8a15Cr08m//faBzmkMIQt3F5vFG0kC/XVe1AFYJqBFkENQhyCOICUgwpjZ+FQVCFoBpBFkENghyCuIAUQ0rvU89Q8M5UlJAykKogVUPKQqqBlIPU68XYzRhjWDwYywf/BSR/UD8yzY+abthYp63RzpUu34o7MJxYow=="
-  (char-to-bp \場)
-  ;; => "0eJyd10FugzAQheG7eE2ljIFgs+w1qixI4laWCImAVo0i7l4Su1U2Xcy/xXyewX5I9s3s+89wGeMwm/Zm4hxOpn16Vph4OA+Tad/WweEYvk0rhZnix9D1dzB0p7CCMbzHIRxf1ncPY5jD6ubr5T7ymHFZdoXpu31YjXl9mnyOfUiT/z/R5TzFOZ6He7m1fFmYq2k3y1KokehQQ5AjyBMkG6QEKYsUWvaELNkrJRJUKqGS9KdEjiBPUM6SVglSFim07KnBSocsQSVBFUENQYL6S6gm/SmRI8gTlKOuVYKURQote0JbghodqgnaEtQQ5AjyBOUoaZUgZZFCGywVUigXqUFHcqFEqZInlZTIEeQJygnUKkHKIoWWPSdQq2qi8iFVeaOokNoi5ZH6PbNrWc1YUsqzvkWqRsohJezDhPWYr5zae4wwViOWM8xuGGpWI5b/GO0h2TJWMlb9sV1hvsI4Pcask6rxtqms96Vf8/MDlgTJtg=="
-  )
+  (println (char-to-bp (first (seq (first args))))))
